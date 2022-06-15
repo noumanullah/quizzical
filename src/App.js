585b8e11ce20a39b11071dcc98757be109236968
Nouman Ullah Siddiqui <nullah@macrosoftinc.com>
2022-06-16 00:02:34 +0500

    quiz answer selection

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Main from "./components/Main"
 import Data from "./data"
 import Quiz from "./components/Quiz"
+import {nanoid} from "nanoid"
 
 function App() {
   const [isQuizStarted, setIsQuizStarted] = React.useState(false)
-  const [quizData] = React.useState(createData())
+  const [quizData, setQuizData] = React.useState(createData())
 
   function bthQuizStart(){
     setIsQuizStarted(true)  
@@ -24,29 +25,57 @@ function App() {
     const cleanData = Data.results.map(quiz =>{
       const arrOptions = []
       arrOptions.push(quiz.correct_answer, ...quiz.incorrect_answers)
-      
+      const arrOptionsMod = arrOptions.map(opt=>{
+        return {
+          id: nanoid(),
+          value: opt,
+          isSelected: false
+        }
+      })
+
       return (
-            {
-              question: quiz.question,
-              options: shuffle(arrOptions)
-            }
+        {
+          id: nanoid(),
+          question: quiz.question,
+          options: shuffle(arrOptionsMod)
+        }
       )
     })
 
     return cleanData
   }
 
-  console.log(quizData)
+  //console.log(quizData)
 
   // React.useEffect(()=>{
-  //   setQuizData(Data.results)
+  //   const arrData = createData()
+  //   setQuizData(arrData)
+  //   console.log("Iam here")
   // },[quizData])
+
+  function handleOptionSelection(questionId, optionId){
+    const updatedData = quizData.map(question => {
+      if(question.id === questionId){
+        
+        const opts = question.options.map(op=>{
+          return {...op, isSelected: op.id === optionId ? true : false} 
+        })
+
+        return {...question, options: opts}
+      }
+      else{
+        return question
+      }
+    })
+
+    setQuizData(updatedData)
+  }
 
   return (
     <div className="App topImage">
       { isQuizStarted === false ? 
         <Main handleClick={bthQuizStart} /> :
-        <Quiz Data={quizData} />
+        <Quiz Data={quizData} handleAnswerClick={handleOptionSelection} />
       }
     </div>
   );
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -6,14 +6,25 @@ export default function Quiz(props){
         return (
         <div key={nanoid()} className="quiz">
             <div className="question">{quiz.question}</div>
-            {quiz.options.map(opt => <button key={nanoid()}>{opt}</button>)}
-            <hr />
+            <div className="btnContainer">
+                {quiz.options.map(opt => <button 
+                                            className={opt.isSelected ? "btnQuizSelected" : "btnQuiz"} 
+                                            onClick={()=>props.handleAnswerClick(quiz.id, opt.id)} 
+                                            key={opt.id}>
+                                                {opt.value}
+                                        </button>)}
+            </div>
+            <hr className="separator" />
         </div>)
     })
 
     return (
         <div>
             {quizData}
+            <div className="resultContainer">
+                <div className="result">You scored 3/5 correct answers</div>
+                <button className="btnSubmit">Check answers</button>
+            </div>
         </div>
     )
 }
