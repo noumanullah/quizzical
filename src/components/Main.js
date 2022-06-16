642b2f5db050fe5dea1b07a9d7260c7fd51100d0
Nouman Ullah Siddiqui <nullah@macrosoftinc.com>
2022-06-17 00:14:58 +0500

    Updated Title

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ export default function Main(props){
     return (
         <header className="App-header">
             <h1 className="headerText">Quizzical</h1>
-            <p className='firstContent'>This is a small quiz application using <a href="_blank">Open Trivia Database</a></p>
+            <p className='firstContent'>A Simple Quiz Game</p>
             <button 
                 className="btnStartQuiz" 
                 onClick={props.handleClick}>Start quiz</button>
